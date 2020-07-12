1e8a0b8e88430c11d8053ca02bbe2cd2dff1c4cb
Young Won <youngwon@Youngs-MacBook-Pro.local>
2020-07-12 11:31:04 -0700

    commit3

---
+++ simplecollectionwithloop/src/forloops/Main.java
@@ -0,0 +1,14 @@
+package forloops;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*** normal loop - count 0 to 9 ***");
+
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
